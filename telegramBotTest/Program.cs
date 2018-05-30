5c7301e39f0cb22b83f1ce2c002551a9d4d6805f
DISCHAIN\Admin <dischain123321@gmail.com>
2018-05-30 23:45:24 +0700

    DI в пустой консольке

--- telegramBotTest/Program.cs
+++ telegramBotTest/Program.cs
@@ -9,18 +9,35 @@ using EFModel;
 using System.ComponentModel;
 using System.Threading;
 using TelegramBot;
+using Autofac;
 
 namespace telegramBotTest
 {
-    internal class Program
+    public static class ContainerConfig
     {
         private static readonly string connStr = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=SQLRepository.UserContext;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
-        static LoymaxTaskBot bot; 
+        public static Autofac.IContainer Configure()
+        {
+            var builder = new ContainerBuilder();
 
+            builder.RegisterType<LoymaxTaskBot>().As<IWebhookBot>();
+            builder.RegisterType<SqlRepository>().As<IRepository>()
+                .WithParameter(new TypedParameter(typeof(string), connStr));
+
+            return builder.Build();
+        }
+    }
+
+    internal class Program
+    {
         static void Main(string[] args)
         {
-            bot = new LoymaxTaskBot(new SQLRepository.SqlRepository(connStr));
-            bot.StartListen();
+            var container = ContainerConfig.Configure();
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var bot = scope.Resolve<IWebhookBot>();
+                bot.StartListen();
+            }
             Console.ReadKey();
         }
     }
